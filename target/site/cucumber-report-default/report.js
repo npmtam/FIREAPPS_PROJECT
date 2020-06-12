92bf03674bad4bfacc737c23e11a38c0aaaa1ef1
npmtam <nguyenphucminhtamqb@gmail.com>
2020-06-12 18:03:27 +0700

    Add more steps on Home page

--- target/site/cucumber-report-default/report.js
+++ target/site/cucumber-report-default/report.js
@@ -283,21 +283,21 @@ formatter.match({
   "location": "WebsiteSteps.i_access_the_website()"
 });
 formatter.result({
-  "duration": 9809236500,
+  "duration": 19512155200,
   "status": "passed"
 });
 formatter.match({
   "location": "WebsiteSteps.i_click_on_get_app_now_button()"
 });
 formatter.result({
-  "duration": 233996000,
+  "duration": 2819845500,
   "status": "passed"
 });
 formatter.match({
   "location": "WebsiteSteps.verify_the_shopify_apps_page_has_been_accessed()"
 });
 formatter.result({
-  "duration": 2101222700,
+  "duration": 1014651400,
   "status": "passed"
 });
 formatter.match({
@@ -310,28 +310,28 @@ formatter.match({
   "location": "WebsiteSteps.i_access_the_something_from_header(String)"
 });
 formatter.result({
-  "duration": 1273844400,
+  "duration": 1055198000,
   "status": "passed"
 });
 formatter.match({
   "location": "WebsiteSteps.i_close_the_messenger_pop_up()"
 });
 formatter.result({
-  "duration": 1247410900,
+  "duration": 1320692300,
   "status": "passed"
 });
 formatter.match({
   "location": "WebsiteSteps.i_check_the_ali_review_app_from_the_slider()"
 });
 formatter.result({
-  "duration": 1063274500,
+  "duration": 1076014200,
   "status": "passed"
 });
 formatter.match({
   "location": "WebsiteSteps.verify_the_Previous_button_is_disabled()"
 });
 formatter.result({
-  "duration": 31812000,
+  "duration": 29614900,
   "status": "passed"
 });
 formatter.match({
@@ -344,7 +344,7 @@ formatter.match({
   "location": "WebsiteSteps.i_take_the_screenshot_as_something(String)"
 });
 formatter.result({
-  "duration": 437234200,
+  "duration": 412570100,
   "status": "passed"
 });
 formatter.match({
@@ -357,7 +357,7 @@ formatter.match({
   "location": "WebsiteSteps.i_click_to_get_app_button_of_something_app(String)"
 });
 formatter.result({
-  "duration": 375505400,
+  "duration": 275156200,
   "status": "passed"
 });
 formatter.match({
@@ -370,14 +370,14 @@ formatter.match({
   "location": "WebsiteSteps.verify_the_something_install_page_appears(String)"
 });
 formatter.result({
-  "duration": 3224133200,
+  "duration": 3152779700,
   "status": "passed"
 });
 formatter.match({
   "location": "WebsiteSteps.i_check_the_ali_hunter_app_from_the_slider()"
 });
 formatter.result({
-  "duration": 1127097200,
+  "duration": 1101610700,
   "status": "passed"
 });
 formatter.match({
@@ -390,7 +390,7 @@ formatter.match({
   "location": "WebsiteSteps.i_take_the_screenshot_as_something(String)"
 });
 formatter.result({
-  "duration": 455256200,
+  "duration": 397227900,
   "status": "passed"
 });
 formatter.match({
@@ -403,7 +403,7 @@ formatter.match({
   "location": "WebsiteSteps.i_click_to_get_app_button_of_something_app(String)"
 });
 formatter.result({
-  "duration": 332224300,
+  "duration": 465879500,
   "status": "passed"
 });
 formatter.match({
@@ -416,14 +416,14 @@ formatter.match({
   "location": "WebsiteSteps.verify_the_something_install_page_appears(String)"
 });
 formatter.result({
-  "duration": 1639698900,
+  "duration": 4551102100,
   "status": "passed"
 });
 formatter.match({
   "location": "WebsiteSteps.i_check_the_ali_orders_app_from_the_slider()"
 });
 formatter.result({
-  "duration": 2129310400,
+  "duration": 2103843500,
   "status": "passed"
 });
 formatter.match({
@@ -436,7 +436,7 @@ formatter.match({
   "location": "WebsiteSteps.i_take_the_screenshot_as_something(String)"
 });
 formatter.result({
-  "duration": 401997700,
+  "duration": 375724400,
   "status": "passed"
 });
 formatter.match({
@@ -449,7 +449,7 @@ formatter.match({
   "location": "WebsiteSteps.i_click_to_get_app_button_of_something_app(String)"
 });
 formatter.result({
-  "duration": 344136700,
+  "duration": 293809400,
   "status": "passed"
 });
 formatter.match({
@@ -462,21 +462,21 @@ formatter.match({
   "location": "WebsiteSteps.verify_the_something_install_page_appears(String)"
 });
 formatter.result({
-  "duration": 4412517800,
+  "duration": 4201837800,
   "status": "passed"
 });
 formatter.match({
   "location": "WebsiteSteps.i_check_the_sales_box_app_from_the_slider()"
 });
 formatter.result({
-  "duration": 1119132600,
+  "duration": 1147012300,
   "status": "passed"
 });
 formatter.match({
   "location": "WebsiteSteps.verify_the_next_button_is_disabled()"
 });
 formatter.result({
-  "duration": 16785000,
+  "duration": 18831200,
   "status": "passed"
 });
 formatter.match({
@@ -489,7 +489,7 @@ formatter.match({
   "location": "WebsiteSteps.i_take_the_screenshot_as_something(String)"
 });
 formatter.result({
-  "duration": 400766400,
+  "duration": 424380800,
   "status": "passed"
 });
 formatter.match({
@@ -502,7 +502,7 @@ formatter.match({
   "location": "WebsiteSteps.i_click_to_get_app_button_of_something_app(String)"
 });
 formatter.result({
-  "duration": 422654900,
+  "duration": 359441600,
   "status": "passed"
 });
 formatter.match({
@@ -515,14 +515,14 @@ formatter.match({
   "location": "WebsiteSteps.verify_the_something_install_page_appears(String)"
 });
 formatter.result({
-  "duration": 1815081900,
+  "duration": 1622159300,
   "status": "passed"
 });
 formatter.match({
   "location": "WebsiteSteps.i_back_to_home_page()"
 });
 formatter.result({
-  "duration": 641359600,
+  "duration": 960160300,
   "status": "passed"
 });
 formatter.match({
@@ -535,7 +535,7 @@ formatter.match({
   "location": "WebsiteSteps.i_wait_for_testimonial_slide_something_display_and_take_the_screenshot(String)"
 });
 formatter.result({
-  "duration": 1550663200,
+  "duration": 1484249600,
   "status": "passed"
 });
 formatter.match({
@@ -548,7 +548,7 @@ formatter.match({
   "location": "WebsiteSteps.i_wait_for_testimonial_slide_something_display_and_take_the_screenshot(String)"
 });
 formatter.result({
-  "duration": 4179872500,
+  "duration": 3908217700,
   "status": "passed"
 });
 formatter.match({
@@ -561,7 +561,7 @@ formatter.match({
   "location": "WebsiteSteps.i_wait_for_testimonial_slide_something_display_and_take_the_screenshot(String)"
 });
 formatter.result({
-  "duration": 4745423800,
+  "duration": 4901566100,
   "status": "passed"
 });
 formatter.match({
@@ -574,7 +574,7 @@ formatter.match({
   "location": "WebsiteSteps.i_wait_for_testimonial_slide_something_display_and_take_the_screenshot(String)"
 });
 formatter.result({
-  "duration": 4831241400,
+  "duration": 4693952500,
   "status": "passed"
 });
 formatter.match({
@@ -587,7 +587,7 @@ formatter.match({
   "location": "WebsiteSteps.i_wait_for_testimonial_slide_something_display_and_take_the_screenshot(String)"
 });
 formatter.result({
-  "duration": 4811673800,
+  "duration": 4778162400,
   "status": "passed"
 });
 formatter.match({
@@ -600,7 +600,7 @@ formatter.match({
   "location": "WebsiteSteps.i_wait_for_testimonial_slide_something_display_and_take_the_screenshot(String)"
 });
 formatter.result({
-  "duration": 4773347100,
+  "duration": 4825993700,
   "status": "passed"
 });
 formatter.match({
@@ -613,7 +613,7 @@ formatter.match({
   "location": "WebsiteSteps.i_wait_for_testimonial_slide_something_display_and_take_the_screenshot(String)"
 });
 formatter.result({
-  "duration": 5001768600,
+  "duration": 4384416400,
   "status": "passed"
 });
 formatter.match({
@@ -626,7 +626,7 @@ formatter.match({
   "location": "WebsiteSteps.i_wait_for_testimonial_slide_something_display_and_take_the_screenshot(String)"
 });
 formatter.result({
-  "duration": 4646058900,
+  "duration": 5213848000,
   "status": "passed"
 });
 formatter.match({
@@ -637,4 +637,46 @@ formatter.match({
     }
   ],
   "location": "WebsiteSteps.i_check_the_something_blog_post_at_home_page_work_well(String)"
+});
+formatter.result({
+  "duration": 36919615100,
+  "error_message": "org.openqa.selenium.TimeoutException: Expected condition failed: waiting for visibility of element located by By.xpath: //div[@class\u003d\u0027col-12 col-md-4\u0027][1st]//h4/a (tried for 30 second(s) with 500 milliseconds interval)\r\n\tat org.openqa.selenium.support.ui.WebDriverWait.timeoutException(WebDriverWait.java:95)\r\n\tat org.openqa.selenium.support.ui.FluentWait.until(FluentWait.java:272)\r\n\tat commons.AbstractPage.waitToElementVisible(AbstractPage.java:340)\r\n\tat pageObject.WebsitePO.checkBlogPostAtHomePage(WebsitePO.java:384)\r\n\tat stepDefinitions.WebsiteSteps.i_check_the_something_blog_post_at_home_page_work_well(WebsiteSteps.java:268)\r\n\tat ✽.And I check the \"1st\" blog post at Home page work well(WebsiteTestcases.feature:58)\r\nCaused by: org.openqa.selenium.InvalidSelectorException: invalid selector: Unable to locate an element with the xpath expression //div[@class\u003d\u0027col-12 col-md-4\u0027][1st]//h4/a because of the following error:\nSyntaxError: Failed to execute \u0027evaluate\u0027 on \u0027Document\u0027: The string \u0027//div[@class\u003d\u0027col-12 col-md-4\u0027][1st]//h4/a\u0027 is not a valid XPath expression.\n  (Session info: chrome\u003d83.0.4103.97)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/invalid_selector_exception.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DESKTOP-OT2B2U6\u0027, ip: \u0027192.168.0.129\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_211\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 83.0.4103.97, chrome: {chromedriverVersion: 83.0.4103.39 (ccbf011cb2d2b..., userDataDir: C:\\Users\\pc\\AppData\\Local\\T...}, goog:chromeOptions: {debuggerAddress: localhost:63362}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:virtualAuthenticators: true}\nSession ID: 56322a48baaf1ff58f00e372036a3337\n*** Element info: {Using\u003dxpath, value\u003d//div[@class\u003d\u0027col-12 col-md-4\u0027][1st]//h4/a}\r\n\tat sun.reflect.GeneratedConstructorAccessor12.newInstance(Unknown Source)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat org.openqa.selenium.support.ui.ExpectedConditions$7.apply(ExpectedConditions.java:205)\r\n\tat org.openqa.selenium.support.ui.ExpectedConditions$7.apply(ExpectedConditions.java:201)\r\n\tat org.openqa.selenium.support.ui.FluentWait.until(FluentWait.java:249)\r\n\tat commons.AbstractPage.waitToElementVisible(AbstractPage.java:340)\r\n\tat pageObject.WebsitePO.checkBlogPostAtHomePage(WebsitePO.java:384)\r\n\tat stepDefinitions.WebsiteSteps.i_check_the_something_blog_post_at_home_page_work_well(WebsiteSteps.java:268)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat cucumber.runtime.Utils$1.call(Utils.java:40)\r\n\tat cucumber.runtime.Timeout.timeout(Timeout.java:16)\r\n\tat cucumber.runtime.Utils.invoke(Utils.java:34)\r\n\tat cucumber.runtime.java.JavaStepDefinition.execute(JavaStepDefinition.java:38)\r\n\tat cucumber.runtime.StepDefinitionMatch.runStep(StepDefinitionMatch.java:37)\r\n\tat cucumber.runtime.Runtime.runStep(Runtime.java:300)\r\n\tat cucumber.runtime.model.StepContainer.runStep(StepContainer.java:44)\r\n\tat cucumber.runtime.model.StepContainer.runSteps(StepContainer.java:39)\r\n\tat cucumber.runtime.model.CucumberScenario.run(CucumberScenario.java:44)\r\n\tat cucumber.runtime.junit.ExecutionUnitRunner.run(ExecutionUnitRunner.java:102)\r\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:63)\r\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:18)\r\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:238)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:63)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:236)\r\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:53)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:229)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:309)\r\n\tat cucumber.runtime.junit.FeatureRunner.run(FeatureRunner.java:70)\r\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:95)\r\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:38)\r\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:238)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:63)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:236)\r\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:53)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:229)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:309)\r\n\tat cucumber.api.junit.Cucumber.run(Cucumber.java:100)\r\n\tat org.junit.runner.JUnitCore.run(JUnitCore.java:160)\r\n\tat com.intellij.junit4.JUnit4IdeaTestRunner.startRunnerWithArgs(JUnit4IdeaTestRunner.java:68)\r\n\tat com.intellij.rt.junit.IdeaTestRunner$Repeater.startRunnerWithArgs(IdeaTestRunner.java:33)\r\n\tat com.intellij.rt.junit.JUnitStarter.prepareStreamsAndStart(JUnitStarter.java:230)\r\n\tat com.intellij.rt.junit.JUnitStarter.main(JUnitStarter.java:58)\r\n",
+  "status": "failed"
+});
+formatter.match({
+  "location": "WebsiteSteps.i_return_to_home_page()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "2nd",
+      "offset": 13
+    }
+  ],
+  "location": "WebsiteSteps.i_check_the_something_blog_post_at_home_page_work_well(String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "WebsiteSteps.i_return_to_home_page()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "3rd",
+      "offset": 13
+    }
+  ],
+  "location": "WebsiteSteps.i_check_the_something_blog_post_at_home_page_work_well(String)"
+});
+formatter.result({
+  "status": "skipped"
+});
 });
